c4567d220d5db206e4cdca14c1670027d15a4b06
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-11-24 12:39:39 -0500

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -1,1 +1,6 @@
+import csv
 
+def save_job_data_to_csv(job_data):
+  with open('job_data.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=['job_title', 'company_name', 'location', 'description', 'application_link'])
+    write.write
