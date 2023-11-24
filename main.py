9048bc7bab69bfa72a74dd372f98f2894b446a17
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-11-24 12:36:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import google_jobs_scraper
+import linkedin_jobs_scraper
+import data_handler
+import scheduler
 
+def main():
+  # Get user input title
+  # Scrape job postings from Google Jobs
+  # Scrape job postings from Linkedin
+  # Combine scraped data
+  # Store combined data in CSV file
+  # Schedule scraping process
