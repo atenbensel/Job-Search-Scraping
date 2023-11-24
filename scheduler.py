223c91038792dd8a0994b67d47a505be161cb7cb
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-11-24 13:12:19 -0500

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,11 @@
+from google_jobs_scraper import scrape_google_jobs
+from linkedin_jobs_scraper import scrape_linkedin_jobs
 
+def job_scraping_job():
+  print("Running job scraping...")
+  scrape_google_jobs()
+  scrape_linkedin_jobs()
+
+def schedule_job_scraping():
+  scheduler = BlockingScheduler()
+  # schedule to run every day at midnight?
