8cb869e8af1e976a50300513e08182c560a0b65a
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-11-24 13:03:43 -0500

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -4,3 +4,9 @@ def save_to_csv(data, filename, mode='w'):
   with open(filename, mode, newline='', encoding='utf-8') as file:
     fieldnames = ['Job Title', 'Company Name', 'Location', 'Job Description', 'Application Link'])
     writer = csv.DictWriter, fieldnames=fieldnames
+    
+    if mode == 'w':
+      writer.writeheader()
+
+    for row in data:
+      writer.writerow(row)
