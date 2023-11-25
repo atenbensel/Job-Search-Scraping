01d97c97001062ef624ac97f147de9762d60d3a4
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-11-25 17:02:37 -0500

    Update frontend.js

--- frontend.js
+++ frontend.js
@@ -1,1 +1,7 @@
+function startJobSearch() {
+    var jobTitle = document.getElementById('jobTitle').value;
 
+    // Add logic here to send the job title to the backend for processing
+    // For simplicity, I'm just adding log the job title for now
+    console.log('Searching for jobs with title:', jobTitle);
+}
