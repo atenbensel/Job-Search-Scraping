3718edc6d0e19e1bb1578dc3e23869bdde54e4d8
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2023-11-24 21:01:57 -0500

    Update google_job_scraper.py

--- google_job_scraper.py
+++ google_job_scraper.py
@@ -1,1 +1,15 @@
+import csv
+import requests
+import bs4 import BeautifulSoup
+from data_handler import save_to_csv
 
+def scrape_google_jobs:
+# google scraping jobs logic here
+
+  job_data = []
+
+  job_data.append({
+    'Job Title':'Software Engineer',
+  })
+
+  save_to_csv(job_data, 'job_data.csv', 'a')
